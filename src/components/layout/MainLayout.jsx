07c032d6d1208fea8f0299c75e667421fe7b9545
Vinicius Mendes <vmcmendes16@hotmail.com>
2026-01-14 19:28:13 -0300

    wip: mais uma tentativa de fix, tentando focar na v 4

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,6 +1,6 @@
 export default function MainLayout({ children }) {
   return (
-    <div className="min-h-screen mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
+    <div className="min-h-screen mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
       {children}
     </div>
   );
