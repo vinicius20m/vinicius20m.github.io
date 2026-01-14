9443c894e53a98fe01e82fc05c54dbc6b700f7e1
Vinicius Mendes <vmcmendes16@hotmail.com>
2026-01-14 16:06:07 -0300

    fixing: teste msg show on index screen. config fix on vite config

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 export default function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-sky-100">
-      <p className="text-[800px] !text-green-400 rotate-6">
-  OK
-</p>
-
+        <p className="text-[500px] !text-green-400 rotate-6">
+            Testeee
+        </p>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/vinicius20m.github.io/",
+  base: "/",
+  // base: "/vinicius20m.github.io/",
 })
