4130a28ea6b320c70853a809baf35bc17ce7ac32
Vinicius Mendes <vmcmendes16@hotmail.com>
2026-02-08 21:15:58 -0300

    fix: header layout style and position

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
 export default function MainLayout({ children }) {
   return (
     <div className="min-h-screen mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
